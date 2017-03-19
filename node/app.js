44dc578c98acb096536e283dedf4510e8721fdd2
Sparshith Sampath <sparshithsampath@gmail.com>
2017-03-19 22:08:09 +0530

    cleanup

--- node/app.js
+++ node/app.js
@@ -142,30 +142,21 @@ function verifyRequestSignature(req, res, buf) {
   }
 }
 
-/*
- * Message Event
- *
- * This event is called when a message is sent to your page. The 'message' 
- * object format can vary depending on the kind of message that was received.
- * Read more at https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-received
- *
- * For this example, we're going to echo any text that we get. If we get some 
- * special keywords ('button', 'generic', 'receipt'), then we'll send back
- * examples of those bubbles to illustrate the special message bubbles we've 
- * created. If we receive a message with an attachment (image, video, audio), 
- * then we'll simply confirm that we've received the attachment.
- * 
- */
+function getAllQuickReplies(callback) {
+  jsonfile.readFile(quickRepliesPath, function(err, obj) {
+    if(err) {
+      return callback&&callback(err);
+    }
+    return callback&&callback(null, obj);
+  });
+}
+
+
 function receivedMessage(event) {
   var senderID = event.sender.id;
   var recipientID = event.recipient.id;
   var timeOfMessage = event.timestamp;
   var message = event.message;
-
-  //  console.log("Received message for user %d and page %d at %d with message:", 
-  //   senderID, recipientID, timeOfMessage);
-  // console.log(JSON.stringify(message));
-
   var isEcho = message.is_echo;
   var messageId = message.mid;
   var appId = message.app_id;
@@ -187,27 +178,23 @@ function receivedMessage(event) {
       messageId, quickReplyPayload);
 
     //Checking if quickReply exists 
-  jsonfile.readFile(quickRepliesPath, function(err, obj) {
-    if(err) {
-      return callback&&callback(err);
+    var quickReplyObjectFetched = function(err, allQuickReplies) {
+      if(err) {
+        return callback&&callback(err);
+      }
+    
+      if(quickReplyPayload in allQuickReplies) {
+       sendQuickReply(senderID, quickReplyPayload);
+      }
     }
     
-    if((quickReplyPayload in obj)) 
-    {
-     sendQuickReply(senderID, quickReplyPayload);
-    }
-    
-  });
+
     return;
   }
 
   if (messageText) {
-    // If we receive a text message, check to see if it matches any special
-    // keywords and send back the corresponding example. Otherwise, just echo
-    // the text we received.
     switch (messageText) {
       case 'I was abused':
-      conole.log("In I was abused");  
         sendQuickReply(senderID, 'askAbusedTime');
         break;
       case 'call spar':
@@ -227,7 +214,6 @@ function receivedMessage(event) {
 
     switch (attachmentType) {
       case 'location':
-        console.log(attachment.payload.coordinates);
         var lat = attachment.payload.coordinates.lat;
         var lon = attachment.payload.coordinates.long;
         console.log(lat, lon);
@@ -260,9 +246,18 @@ function receivedPostback(event) {
   var timeOfPostback = event.timestamp;
   var payload = event.postback.payload;
 
-  if(payload == 'getStarted' || payload == 'getStartedTalk' || payload == 'getStartedHelp' || payload == 'getStartedAct') {
-    sendQuickReply(senderID, payload);
-  }
+  var quickReplyObjectFetched = function(err, allQuickReplies) {
+    if(err) {
+      console.log(err);
+    }
+
+    if(payload in allQuickReplies) {
+      sendQuickReply(senderID, payload);
+    }
+  };
+
+  getAllQuickReplies(quickReplyObjectFetched);
+
 }
 
 function sendTextMessage(recipientId, messageText) {
@@ -328,14 +323,16 @@ function getUserDetails(userId, callback) {
 }
 
 function getQuickReplyOptions(useCase, callback) {
-  jsonfile.readFile(quickRepliesPath, function(err, obj) {
+
+  var quickReplyObjectFetched = function(err, allQuickReplies) {
     if(err) {
       return callback&&callback(err);
     }
-
-    var replyOptions = obj[useCase];
+    var replyOptions = allQuickReplies[useCase];
     return callback&&callback(null, replyOptions);
-  });
+  };
+
+  getAllQuickReplies(quickReplyObjectFetched);
 }
 
 function sendQuickReply(recipientId, useCase) {
